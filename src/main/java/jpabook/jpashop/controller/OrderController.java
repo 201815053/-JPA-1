879682c9a389dd36183b60753b85ebe6b8e754a2
kyunghyun <80056046+201815053@users.noreply.github.com>
2022-08-22 20:55:19 +0900

    Update OrderController.java

--- src/main/java/jpabook/jpashop/controller/OrderController.java
+++ src/main/java/jpabook/jpashop/controller/OrderController.java
@@ -52,7 +52,7 @@ public class OrderController {
         return "order/orderList";
     }
 
-    @PostMapping("/orders/{orderId}/canc")
+    @PostMapping("/orders/{orderId}/cancel")
     public String cancelOrder(@PathVariable("orderId") Long orderId) {
         orderService.cancelOrder(orderId);
         return "redirect:/orders";
